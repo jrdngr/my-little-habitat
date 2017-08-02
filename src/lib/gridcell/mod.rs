74313c19baefc7f94ffa49fa4b23ae4ca551be79
Jordan Grace <jordan.t.grace@gmail.com>
2017-08-01 22:08:45 -0400

    Prepped GridCell for graphics

--- src/lib/gridcell/mod.rs
+++ src/lib/gridcell/mod.rs
@@ -1,36 +1,26 @@
 use ::lib::grid::Grid;
 
 pub trait GridCell {
-    fn display(&self);
+    fn get_color(&self) -> [f32; 4];
     fn act(&self, grid: &Grid<&GridCell>);
-    fn set_char(&mut self, new_char: char);
-    fn get_char(&self) -> char;
 }
 
-pub struct Zeero {
+pub struct Empty {
     disp: char,   
 }
 
-impl Zeero {
-    pub fn new() -> Zeero {
-        Zeero { disp: '0' }
+impl Empty {
+    pub fn new() -> Self {
+        Self { disp: '0' }
     }
 }
 
-impl GridCell for Zeero {
-    fn display(&self) {
-        print!("{}", self.disp);
+impl GridCell for Empty {
+    fn get_color(&self) -> [f32; 4] {
+        [0.0, 0.0, 0.0, 0.0]
     }
 
     fn act(&self, grid: &Grid<&GridCell>) {
         // Do nothing;
-    }
-
-    fn set_char(&mut self, new_char: char) {
-        self.disp = new_char;
-    }
-
-    fn get_char(&self) -> char {
-        self.disp
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -11,26 +11,5 @@ use lib::grid::Grid;
 use lib::gridcell::*;
 
 fn main() {
-    let mut data: Vec<Rc<RefCell<Box<GridCell>>>> = Vec::new(); 
-    for _ in 0..25 {
-        data.push(Rc::new(RefCell::new(Box::new(Zeero::new()))));
-    }
-    let grid = Grid::with_data(5, 5, data);
-
-    for (i, cell) in grid.iter().enumerate() {
-        if i % 5 == 0 { print!("\n") };
-        cell.borrow().display();
-    }
-
-    {
-        let what = grid[(1, 1)].clone();
-        what.borrow_mut().set_char('1');
-    }
-
-    println!();
-
-    for (i, cell) in grid.iter().enumerate() {
-        if i % 5 == 0 { print!("\n") };
-        cell.borrow().display();
-    }
+    
 }
